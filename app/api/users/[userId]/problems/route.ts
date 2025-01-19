f35951c0406631ede9327bfa9c1ea14cf53081a1
CalvinChiramal <calvin.chiramal@bigbinary.com>
2025-01-19 19:55:56 +0530

    Added GET users and problems API routes

---
+++ app/api/users/[userId]/problems/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ userId: string }> },
+) {
+  const { userId } = await params;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      problems: true,
+    },
+  });
+
+  return Response.json(user?.problems);
+}
