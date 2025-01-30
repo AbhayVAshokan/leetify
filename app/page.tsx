179aed95aa42930525c4d1fba32ff80fc6bd5bbf
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-01-30 21:00:42 +0530

    fix: redirect to the first user's username

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { fetchUsers } from "./actions";
 
 const HomePage = async () => {
   const users = await fetchUsers();
-  redirect(`/${users[0].id}`);
+  redirect(`/${users[0].username}`);
 };
 
 export default HomePage;
