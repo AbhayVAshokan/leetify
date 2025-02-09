7cf23104c06f76ea8ea2f05724de90a0abcfdc11
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-02-09 19:44:03 +0530

    Replaced usage of DIFFICULTY_SCORE with SCORE

--- app/[username]/utils.tsx
+++ app/[username]/utils.tsx
@@ -6,7 +6,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Star } from "lucide-react";
 import Link from "next/link";
 import { Problem } from "@prisma/client";
-import { DIFFICULTY_ORDER } from "./constants";
+import { SCORE } from "@/lib/constants/score";
 
 export const buildColumns = (
   onToggleFavorite: (props: { problemId: string; isFavorite: boolean }) => void,
@@ -54,9 +54,9 @@ export const buildColumns = (
       // Sort difficulty as "easy" => "medium" => "hard".
       // @ts-expect-error: these type errors can be safely ignored since any
       // mismatch is handled in the return statement.
-      const diffA = DIFFICULTY_ORDER[rowA.original.difficulty.toLowerCase()];
+      const diffA = SCORE[rowA.original.difficulty.toLowerCase()];
       // @ts-expect-error: same as above.
-      const diffB = DIFFICULTY_ORDER[rowB.original.difficulty.toLowerCase()];
+      const diffB = SCORE[rowB.original.difficulty.toLowerCase()];
       return diffA - diffB || 0;
     },
   },
