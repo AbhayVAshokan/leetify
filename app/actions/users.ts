44bd1dd70bdacdef2ccaccde5bf2960d71063553
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-02-10 23:32:46 +0530

    Implemented fetchStreak action

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,6 +3,7 @@
 import { SCORE } from "@/lib/constants/score";
 import prisma from "@/lib/prisma";
 import { UserRanked } from "@/types/user";
+import { ONE_DAY_IN_SECONDS, TIMEZONE } from "./constants";
 
 export const fetchUsers = async (): Promise<UserRanked[]> => {
   const users = await prisma.user.findMany({
@@ -24,4 +25,56 @@ export const fetchUsers = async (): Promise<UserRanked[]> => {
     .sort((a, b) => b.score - a.score)
     .map((user, idx) => ({ ...user, rank: idx + 1 }));
   return usersRanked;
+};
+
+const parseDate = (date: Date) =>
+  new Date(date).toLocaleDateString("en-US", { timeZone: TIMEZONE });
+
+export const fetchStreak = async ({
+  username,
+}: {
+  username: string;
+}): Promise<{
+  streakCount: number;
+  currentDayCompleted: boolean;
+}> => {
+  const problems = await prisma.problem.findMany({
+    where: { user: { username } },
+    orderBy: { submittedAt: "desc" },
+  });
+
+  if (problems.length === 0) {
+    return { streakCount: 0, currentDayCompleted: false };
+  }
+
+  const currentDayCompleted =
+    parseDate(problems[0].submittedAt) === parseDate(new Date());
+
+  let streakCount = 0;
+  let prevDate = parseDate(new Date(+new Date() + ONE_DAY_IN_SECONDS));
+  problems.every((problem) => {
+    const currDate = parseDate(problem.submittedAt);
+    if (currDate === prevDate) {
+      return true;
+    }
+
+    const date1 = new Date(currDate);
+    const date2 = new Date(prevDate);
+    // @ts-expect-error: subtraction of two bigints will be an int.
+    const diff = (date2 - date1) / ONE_DAY_IN_SECONDS;
+
+    if (diff === 1) {
+      ++streakCount;
+    } else {
+      return false;
+    }
+
+    prevDate = currDate;
+    return true;
+  });
+
+  return {
+    streakCount: streakCount,
+    currentDayCompleted: currentDayCompleted,
+  };
 };
