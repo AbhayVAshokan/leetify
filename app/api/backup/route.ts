77fb2a7f61b53610f29685092f501c373f174f75
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-03-02 13:36:04 +0530

    Update cron to run everyday at midnight (done with tests). Throw a non-zero status code on API failure

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -2,21 +2,17 @@ import prisma from "@/lib/prisma";
 import zlib from "zlib";
 
 export async function POST() {
-  try {
-    const [users, problems, topics] = await Promise.all([
-      prisma.user.findMany(),
-      prisma.problem.findMany(),
-      prisma.topic.findMany(),
-    ]);
+  const [users, problems, topics] = await Promise.all([
+    prisma.user.findMany(),
+    prisma.problem.findMany(),
+    prisma.topic.findMany(),
+  ]);
 
-    const data = JSON.stringify({ users, problems, topics });
-    return new Response(zlib.gzipSync(data), {
-      headers: {
-        "Content-Type": "text/json",
-        "Content-Encoding": "gzip",
-      },
-    });
-  } catch (error) {
-    return Response.json({ error });
-  }
+  const data = JSON.stringify({ users, problems, topics });
+  return new Response(zlib.gzipSync(data), {
+    headers: {
+      "Content-Type": "text/json",
+      "Content-Encoding": "gzip",
+    },
+  });
 }
